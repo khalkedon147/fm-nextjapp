6c2bc020f9938d4e0af13ff2e2dc697606d2ac1e
khalkedon147 <khalkedon147@gmail.com>
2023-06-14 16:59:44 +0300

    end of day1

---
+++ app/new-user/page.tsx
@@ -0,0 +1,29 @@
+import { prisma } from "@/utils/db"
+import { currentUser } from "@clerk/nextjs"
+import {redirect} from "next/navigation"
+
+const createNewUser = async() => {
+  const user =await currentUser()
+  console.log(user)
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    }
+  })
+  if(!match){
+    await prisma.user.create({
+      data: {clerkId:user.id,
+        email: user?.emailAddresses[0].emailAddress
+      }
+    })
+  }
+  redirect('/journal')
+}
+
+
+export default async function NewUser() {
+  await createNewUser()
+  return <div>
+  ...loading
+  </div>
+}
